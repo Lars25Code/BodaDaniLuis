481df2b00c6acdc4ef82def767c72a42e6a9bf75
luisrosp <luisrosp@suramericana.com.co>
2024-01-04 14:55:43 -0500

    Color Letra Informacion Black

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -4,7 +4,7 @@ export const Information = () => {
   return (
     <>
       <div className="w-full bg-[#F1C0A1] h-24 flex items-center justify-center">
-        <h1 className="w-4/5 text-xl text-white text-center">
+        <h1 className="w-4/5 text-xl text-center">
           ¡Gracias por acompañarnos en este momento tan importante!
         </h1>
       </div>
